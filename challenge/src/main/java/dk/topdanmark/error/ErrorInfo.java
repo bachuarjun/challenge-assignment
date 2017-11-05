febfc08a9aa21407b5dec3701ee776ed672bf955
Arjun Kumar <bachuarjun@gmail.com>
2017-11-06 00:41:05 +0530

    fine tuned error messages and preparing the error messages

--- challenge/src/main/java/dk/topdanmark/error/ErrorInfo.java
+++ challenge/src/main/java/dk/topdanmark/error/ErrorInfo.java
@@ -1,5 +1,7 @@
 package dk.topdanmark.error;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public final class ErrorInfo {
 	
 	private String errorCode;
@@ -25,6 +27,34 @@ public final class ErrorInfo {
 	public String getSystemID() {
 		return systemID;
 	}
-	
+	@JsonIgnore
+	public int getHttpStatusCode(){
+		int status = 500;
+		String errorCode = getErrorCode();
+		
+		switch (errorCode) {
+			case "ERR500":
+				status = 500;
+				 break;
+			case "ERR429":
+				status = 429;
+				 break;
+			case "ERR503":
+				status = 503;
+				 break;
+			case "ERR424":
+				status = 424;
+				 break;
+			case "ERR599":
+				status = 599;
+				 break;
+			case "ERR400":
+				status = 400;
+				 break;
+			default:
+				status = 500;	
+		}
+		return status;
+	}
 	
 }
